3521e678d28862547ffe71f1181ea9eb8b57dff0
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-11-16 17:51:59 -0200

    Buscando o Ruby - parte 2 - Completo

--- lib/cliente.rb
+++ lib/cliente.rb
@@ -1,6 +1,26 @@
 class Cliente
-  attr_reader :nome
-  def initialize(nome: nome)
+  attr_reader :nome, :sobrenome, :cpf
+  #deveria ser possivel criar uma pessoa
+  def initialize(nome: nome, sobrenome: sobrenome, cpf: cpf)
     @nome = nome
+    @sobrenome = sobrenome
+    @cpf = cpf
+  end
+
+  #deveria ter o nome completo com tratamento, quando houver tratamento
+  def tratamento=(trat)
+  	@tratamento = "#{trat}"
+  end
+  def tratamento()
+  	@tratamento
+  end
+
+  #deveria ser possivel obter nome completo
+  def nome_completo
+  	if(@tratamento)
+  		"#{@tratamento} #{@nome} #{@sobrenome}"
+  	else
+  		"#{@nome} #{@sobrenome}"
+  	end
   end
 end
